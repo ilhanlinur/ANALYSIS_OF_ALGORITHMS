c8af5e452ecc3388df222fb6df6a1180499cbe87
ilhanlinur <ilhanlinurr@gmail.com>
2019-05-03 14:02:39 +0300

    hafta10

---
+++ Untitled (1) (1).py
@@ -0,0 +1,54 @@
+
+# coding: utf-8
+
+# In[39]:
+
+def maxVal(toConsider, avail):
+
+    if toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0][2] > avail:
+        result = maxVal(toConsider[1:], avail)
+    else:
+        nextItem = toConsider[0]
+        withVal, withToTake = maxVal(toConsider[1:],avail - nextItem[2])
+        withVal += nextItem[1]
+        withoutVal, withoutToTake = maxVal(toConsider[1:],avail)
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    return result
+
+
+
+# In[40]:
+
+names = ['a', 'b', 'c', 'd']
+vals = [6, 7, 8, 9]
+weights = [3, 3, 2, 5]
+
+
+# In[41]:
+
+def getItems():
+    Items_a=[names[0],vals[0],weights[0]]
+    Items_b=[names[1],vals[1],weights[1]]
+    Items_c=[names[2],vals[2],weights[2]]
+    Items_d=[names[3],vals[3],weights[3]]
+    Items_c
+    Items=[Items_a,Items_b,Items_c,Items_d]
+    return Items
+getItems()
+
+
+# In[42]:
+
+Items=getItems()
+maxVal(Items,10)
+
+
+# In[ ]:
+
+
+
